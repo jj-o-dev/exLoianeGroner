262f3fcdef3359e070579a0297418ceacf95a975
João Paulo R. Rodrigues <86799330+jj-o-dev@users.noreply.github.com>
2025-03-27 16:16:56 -0300

    Done src/aula36/ex1/

---
+++ src/aula36/ex1/Agenda.java
@@ -0,0 +1,43 @@
+package aula36.ex1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agenda {
+    private String nome;
+    private List<Contato> contatos = new ArrayList<>();
+
+    public Agenda(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void addContato(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public String printContato(String nomeContato){
+        for (Contato c: contatos) {
+            if (c.getNome().equals(nomeContato)) {
+                return c.getNome() + c.getNumero() + c.getEmail();
+            }
+        }
+        return "Contato com nome " + nomeContato + " não encontrado.";
+    }
+
+    public String showContatos() {
+        StringBuilder sb = new StringBuilder();
+        for (Contato c: contatos) {
+            sb.append(c.getNome()).append(" ").append(c.getNumero())
+                    .append(" ").append(c.getEmail()).append("\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/aula36/ex1/Main.java
@@ -0,0 +1,32 @@
+package aula36.ex1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Qual o nome da agenda? ");
+        String nome = scan.nextLine();
+        Agenda agenda = new Agenda(nome);
+
+        System.out.print("Quantos contatos deseja inserir: ");
+        int num = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < num; i++) {
+            System.out.println("Insira contato #" + (i+1));
+            System.out.print("Nome: ");
+            String nomeContato = scan.nextLine();
+            System.out.print("Número: ");
+            String numContato = scan.nextLine();
+            System.out.print("Email: ");
+            String emailContato = scan.nextLine();
+
+            agenda.addContato(new Contato(nomeContato, numContato, emailContato));
+        }
+
+        System.out.println();
+        System.out.println(agenda.showContatos());
+
+        scan.close();
+    }
+}
